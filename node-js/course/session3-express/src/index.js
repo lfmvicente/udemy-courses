122b84f3f7eb412a2dd7f18a72391693df40629b
Luis Felipe <lfm.vicente@gmail.com>
2023-01-17 08:26:11 -0300

    feat(routes): developing some routes with params

--- node-js/course/session3-express/src/index.js
+++ node-js/course/session3-express/src/index.js
@@ -1,12 +1,40 @@
-const express = require("express"); //importando express
+const express = require('express'); //importando express
 const app = express(); //funçao que inicia o express
+
+app.get('/', function(request, response){
+    response.send('Welcome to my app!');   
+})
+
+//route with mandatory param
+app.get('/:name', function(request, response){
+    let name = request.params.name
+    response.send(name + ', welcome to my app!')
+})
+
+//route with optional param
+app.get('/blog/:article?', function(request, response){
+    let article = request.params.article
+    if (article) {
+        response.send('Welcome to ' + article + ' session')
+    }
+    response.send('Welcome to my blog home!')
+})
+
+//route with query param (insecure, not often used)
+app.get('/channel/youtube', function(request, response){
+    let channel = request.query['channel']
+    if (channel) {
+        response.send('<h2>Welcome to ' + channel + '\'s channel <\/h2>')
+    }
+    response.send('<h1>Welcome to my Youtube channel session</h1>')
+})
 
 app.listen(8181,
     function(error){
         if (error) {
-            console.log("Server Error");
+            console.log('Server Error');
         } else {
-            console.log("Server Online");
+            console.log('Server Online');
         }
     }
 )
