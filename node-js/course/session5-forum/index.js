c28d3a2f24a9aaeaae299dae9e08cb537c5c93ba
Luis Felipe <lfm.vicente@gmail.com>
2023-01-23 08:45:01 -0300

    feat(express): using if and forEach

--- node-js/course/session5-forum/index.js
+++ node-js/course/session5-forum/index.js
@@ -3,15 +3,29 @@ const app = express();
 
 //use ejs as engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    response.send("Please, pass parameters on URL")
+})
 
 app.get('/:firstname/:lang', function(request, response){
     let firstname = request.params.firstname;
     let lang = request.params.lang
+    let success = false;
+
+    let products = [
+        {name: 'Playstation 5', price: 500.0},
+        {name: 'Gran Turismo', price: 50.0},
+        {name: 'Resident Evil 4', price: 500.0}
+    ]
     
     response.render('index', {
         firstname: firstname,
         lang: lang,
-        company: 'My Company'
+        company: 'My Company',
+        success: success,
+        products: products
     });
 });
 
