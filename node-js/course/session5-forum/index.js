65bc1280e5bb79f935315eca20bc1e8ac8cbbc83
Luis Felipe <lfm.vicente@gmail.com>
2023-01-24 08:55:29 -0300

    feat(bootstrap): add bootstrap to project

--- node-js/course/session5-forum/index.js
+++ node-js/course/session5-forum/index.js
@@ -1,32 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //use ejs as engine
 app.set('view engine', 'ejs');
+//configure views path
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
 
-app.get('/', function(request, response){
-    response.send("Please, pass parameters on URL")
-})
+app.get('/', (request, response) => {
+    response.render('index');
+});
 
-app.get('/:firstname/:lang', function(request, response){
-    let firstname = request.params.firstname;
-    let lang = request.params.lang
-    let success = false;
-
-    let products = [
-        {name: 'Playstation 5', price: 500.0},
-        {name: 'Gran Turismo', price: 50.0},
-        {name: 'Resident Evil 4', price: 500.0}
-    ]
-    
-    response.render('index', {
-        firstname: firstname,
-        lang: lang,
-        company: 'My Company',
-        success: success,
-        products: products
-    });
+app.get('/ask', (request, response) => {
+    response.render('ask');
 });
 
 app.listen(8080, ()=>{
