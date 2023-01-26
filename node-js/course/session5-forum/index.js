9efd1a25d655f6c0539fa170182cb1efa0f83330
Luis Felipe <lfm.vicente@gmail.com>
2023-01-26 08:46:51 -0300

    feat(express): using partials and send form data to node

--- node-js/course/session5-forum/index.js
+++ node-js/course/session5-forum/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 //use ejs as engine
 app.set('view engine', 'ejs');
@@ -8,12 +9,25 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
 
+//bodyParser configurations
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json);
+
+//routes
 app.get('/', (request, response) => {
     response.render('index');
 });
 
 app.get('/ask', (request, response) => {
     response.render('ask');
+});
+
+app.post('/save-question', (request, response) => {
+    let title = request.body.title
+    let description = request.body.description
+    response.send("Title: ")
 });
 
 app.listen(8080, ()=>{
