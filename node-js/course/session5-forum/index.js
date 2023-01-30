1ae395eaca2e3612f80df1c93fefc53620276a89
Luis Felipe <lfm.vicente@gmail.com>
2023-01-30 08:31:37 -0300

    feat(sequelize): using models to create tables

--- node-js/course/session5-forum/index.js
+++ node-js/course/session5-forum/index.js
@@ -2,22 +2,38 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const connection = require('./db/database');
+const Question = require('./db/Question');
+
+//DB connection promise
+connection
+    .authenticate()
+    .then(() =>{
+        console.log("Db connect success");
+    })
+    .catch((exception) => {
+        console.log(exception);
+    })
 
 //use ejs as engine
 app.set('view engine', 'ejs');
 //configure views path
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
-
 //bodyParser configurations
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //routes
 app.get('/', (request, response) => {
-    response.render('index');
+    Question.findAll({ raw: true, order:[
+        ['id', 'DESC']
+    ]}).then((questions) => {
+        response.render('index', {
+            questions: questions
+        });
+        console.log(questions);
+    })
 });
 
 app.get('/ask', (request, response) => {
@@ -27,10 +43,15 @@ app.get('/ask', (request, response) => {
 app.post('/save-question', (request, response) => {
     let title = request.body.title
     let description = request.body.description
-    response.send("Title: ")
+    Question.create({
+        title: title,
+        description: description
+    }).then(() => {
+        response.redirect('/');
+    })
 });
 
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log('Server Running');
 });
 
