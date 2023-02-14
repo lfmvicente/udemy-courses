e68f9c387ba2ca8ea11204cc6f88d83d2dc94767
Luis Felipe <lfm.vicente@gmail.com>
2023-02-14 09:32:02 -0300

    feat(blog): add delete action to categories

--- node-js/course/session7-blog/categories/CategoriesController.js
+++ node-js/course/session7-blog/categories/CategoriesController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Category = require('./Category');
 const slugify = require('slugify');
+const { response } = require('express');
 
 router.get('/admin/categories/new', (request, response) => {
     response.render('admin/categories/new');
@@ -26,6 +27,25 @@ router.get('/admin/categories', (request, response) => {
     Category.findAll().then((categories) => {
         response.render('admin/categories/index', {categories: categories});  
     })
+})
+
+router.post('/categories/delete', (request, response) => {
+    let id = request.body.id
+    if (id) {
+        if (!isNaN(id)) { //verifica se o id é numérico
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                response.redirect('/admin/categories')
+            })
+        } else {
+            response.redirect('/admin/categories')
+        }
+    } else {
+        response.redirect('/admin/categories')
+    }
 })
 
 module.exports = router;
