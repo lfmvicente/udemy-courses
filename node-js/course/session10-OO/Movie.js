e4be9e6eae5aed152a8e1b855c17c8407ae8f7cd
Luis Felipe <lfm.vicente@gmail.com>
2023-03-23 08:47:36 -0300

    feat(OO): OO objects

--- node-js/course/session10-OO/Movie.js
+++ node-js/course/session10-OO/Movie.js
@@ -1,12 +1,19 @@
+//Movie Class
 class Movie
 {
-    constructor() {
-        this.title = ''
-        this.year = 0
-        this.director = ''
-        this.gender = ''
+    constructor(
+        title, 
+        year, 
+        director, 
+        gender,
+        duration
+    ) {
+        this.title = title
+        this.year = year
+        this.director = director
+        this.gender = gender
         this.actors = []
-        this.duration = 0
+        this.duration = duration
     }
 
     Reproduce() {
@@ -28,4 +35,38 @@ class Movie
     Close() {
         console.log('Close X')
     }
+
+    Info() {
+        console.log(
+            `
+                Title: ${this.title}
+                Year: ${this.year}
+                Director: ${this.director}
+            `
+        )
+    }
 }
+
+//Movie Objects
+let Avengers = new Movie(
+    'Ultimate Avengers',
+    2019,
+    'Anthony Russo',
+    'Action',
+    120
+);
+
+Avengers.Info();
+Avengers.Reproduce();
+
+let Batman = new Movie(
+    'The Batman',
+    2021,
+    'Matt Reeves',
+    'Action',
+    ['Robert'],
+    120
+);
+
+Batman.Info();
+Batman.Reproduce();
